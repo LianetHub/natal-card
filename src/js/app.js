4ce2e9482fcc0fdaa513b443ef564ff85287c4dd
lianet <jack.tolmak@yandex.ru>
2024-07-09 20:30:01 +0300

    commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,136 +2,21 @@
 
 
 
-import * as devFunctions from './modules/functions.js';
 
-//  init Fancybox
-// if (typeof Fancybox !== "undefined" && Fancybox !== null) {
-//     Fancybox.bind("[data-fancybox]", {
-//         dragToClose: false,
-//         closeButton: false
-//     });
-// }
+$(document).ready(function () {
 
-document.addEventListener('DOMContentLoaded', () => {
-
-    devFunctions.OS();
-    devFunctions.isWebp();
-    // devFunctions.checkEmptyInputs()
-
-
-
-
-
-    // event handlers
-
-    document.addEventListener('click', (e) => {
-
-        const target = e.target;
-
-        if (target.matches('.form__field-clear')) {
-            target.previousElementSibling.value = "";
-            target.previousElementSibling.classList.remove('_input');
-        }
-
-        if (target.closest('.icon-menu')) {
-            getMenu()
-        }
-
-        if (target.classList.contains('products__filters-caption')) {
-            target.classList.toggle('active');
-            target.nextElementSibling.classList.toggle('active');
-        }
-
-
-    });
-
-    function getMenu() {
-        document.body.classList.toggle('menu-lock');
-        document.querySelector('.header').classList.toggle('open-menu');
-    }
-
-
-    // function getIndexInParent(node) {
-    //     var children = node.parentNode.childNodes;
-    //     var num = 0;
-    //     for (var i = 0; i < children.length; i++) {
-    //         if (children[i] == node) return num;
-    //         if (children[i].nodeType == 1) num++;
-    //     }
-    //     return -1;
-    // }
-
-
-
-
-    //  sliders
-
-    if (document.querySelectorAll('.slider')) {
-        document.querySelectorAll('.slider').forEach(slider => {
-            new Swiper(slider, {
-                slidesPerView: 1,
-                navigation: {
-                    nextEl: slider.querySelector('.slider__next'),
-                    prevEl: slider.querySelector('.slider__prev')
-                },
-                pagination: {
-                    el: slider.querySelector('.slider__pagination'),
-                    type: "fraction",
-                    renderFraction: function (currentClass, totalClass) {
-
-
-                        return '<span class="' + currentClass + '"></span>' +
-                            '/' +
-                            '<span class="' + totalClass + '"></span>';
-                    },
-                    formatFractionCurrent: function (number) {
-                        return addLeadingZero(number);
-                    },
-                    formatFractionTotal: function (number) {
-                        return addLeadingZero(number);
-                    }
-                },
-            })
-            function addLeadingZero(number) {
-                return number < 10 ? '0' + number : number;
+    if ($('#date').length > 0) {
+        new Pikaday({
+            field: document.getElementById('date'),
+            format: 'YYYY-MM-DD',
+            firstDay: 1,
+            i18n: {
+                previousMonth: 'Предыдущий месяц',
+                nextMonth: 'Следующий месяц',
+                months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+                weekdays: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'],
+                weekdaysShort: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
             }
-
-        })
+        });
     }
-
-
-    // animation header
-
-    // window.addEventListener('scroll', function (e) {
-
-    //     document.body.style.setProperty("--header-height", Math.ceil(document.querySelector('.header__wrapper').offsetHeight) + "px")
-    // })
-
-
-    // const headerElement = document.querySelector('.header');
-
-    // const callback = function (entries, observer) {
-    //     if (entries[0].isIntersecting) {
-    //         headerElement.classList.remove('scroll');
-    //     } else {
-    //         headerElement.classList.add('scroll');
-    //     }
-    // };
-
-    // const headerObserver = new IntersectionObserver(callback);
-    // headerObserver.observe(headerElement);
-
-
-
-
-    // configurator
-
-
-
-
-
-})
-
-
-
-
+});
